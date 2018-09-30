7d9126d3d0083cf3d57bae573487ea279d9f968d
Anton Nemirov <ant.nemirov@gmail.com>
2018-09-30 11:41:18 +0300

    update

--- match_nmatch/ex00/match.c
+++ match_nmatch/ex00/match.c
@@ -5,24 +5,23 @@ int rmatch(char *s1, char *s2, int pos1, int pos2)
 {
     if (s2[pos2] == '*' && s2[pos2 + 1] == '*')
         return rmatch(s1, s2, pos1, pos2 + 1);
-    if (s1[pos1] == '\0' || s2[pos2] == '\0')
+    if (s1[pos1] == '\0')
     {
-        if ((s1[pos1] == '\0' && s2[pos2] == '\0') || (s2[pos2] == '*' && s2[pos2 + 1] == '\0'))
+        if (s2[pos2] == '\0' || (s2[pos2] == '*' && s2[pos2 + 1] == '\0'))
             return (1);
         else
             return (0);
     }
+    else if (s2[pos2] == '\0')
+        return (0);
     if (s1[pos1] == s2[pos2])
         return rmatch(s1, s2, pos1 + 1, pos2 + 1);
     else
     {
         if (s2[pos2] == '*')
         {
-            if (s1[pos1] == s2[pos2 + 1])
-            {
-                if (rmatch(s1, s2, pos1, pos2 + 1))
-                    return (1);
-            }
+            if (s1[pos1] == s2[pos2 + 1] && rmatch(s1, s2, pos1, pos2 + 1))
+                return (1);
             return rmatch(s1, s2, pos1 + 1, pos2);
         }
     }
