3c15afb67294d7c0667a6131d6f10bbdbb1c9d89
Anton Nemirov <ant.nemirov@gmail.com>
2018-09-30 13:00:45 +0300

    init main for tests

--- match_nmatch/main.c
+++ match_nmatch/main.c
@@ -1,7 +1,21 @@
 #include "ex00/match.c"
+#include <stdio.h>
 
 int main()
 {
-    match("Hello", "Hello");
+    printf("Test nmatch:\n");
+    printf("Test values \"aaa\" and \"aaa\": %s\n", match("aaa", "aaa") ? "OK" : "ERROR");
+    printf("Test values \"aaa\" and \"*\": %s\n", match("aaa", "*") ? "OK" : "ERROR");
+    printf("Test values \"\" and \"\": %s\n", match("", "") ? "OK" : "ERROR");
+    printf("Test values \"*\" and \"*\": %s\n", match("*", "*") ? "OK" : "ERROR");
+    printf("Test values \"\" and \"*\": %s\n", match("", "*") ? "OK" : "ERROR");
+    printf("Test values \"aaa\" and \"****\": %s\n", match("aaa", "****") ? "OK" : "ERROR");
+    printf("Test values \"aaa\" and \"aaa****\": %s\n", match("aaa", "aaa****") ? "OK" : "ERROR");
+    printf("Test values \"aaa\" and \"*aaa****\": %s\n", match("aaa", "*aaa****") ? "OK" : "ERROR");
+    printf("Test values \"aaa\" and \"a*a*a\": %s\n", match("aaa", "a*a*a") ? "OK" : "ERROR");
+    printf("Test values \"aaa\" and \"a*b*a\": %s\n", match("aaa", "a*b*a") ? "ERROR" : "OK");
+    printf("Test values \"Foo Bar Baz\" and \"Fo********ah\": %s\n", match("Foo Bar Baz", "Fo********ah") ? "ERROR" : "OK");
+
+    getchar();
     return (0);
 }
