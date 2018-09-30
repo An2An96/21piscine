726fadb3115612a1e3e5aa2729e053bffcf149e8
Anton Nemirov <ant.nemirov@gmail.com>
2018-09-30 11:39:56 +0300

    complete ex00 (match)

--- match_nmatch/ex00/match.c
+++ match_nmatch/ex00/match.c
@@ -1,31 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int rmatch(char *s1, char *s2, int pos1, int pos2)
+{
+    if (s2[pos2] == '*' && s2[pos2 + 1] == '*')
+        return rmatch(s1, s2, pos1, pos2 + 1);
+    if (s1[pos1] == '\0' || s2[pos2] == '\0')
+    {
+        if ((s1[pos1] == '\0' && s2[pos2] == '\0') || (s2[pos2] == '*' && s2[pos2 + 1] == '\0'))
+            return (1);
+        else
+            return (0);
+    }
+    if (s1[pos1] == s2[pos2])
+        return rmatch(s1, s2, pos1 + 1, pos2 + 1);
+    else
+    {
+        if (s2[pos2] == '*')
+        {
+            if (s1[pos1] == s2[pos2 + 1])
+            {
+                if (rmatch(s1, s2, pos1, pos2 + 1))
+                    return (1);
+            }
+            return rmatch(s1, s2, pos1 + 1, pos2);
+        }
+    }
+    return (0);
+}
+
 int match(char *s1, char *s2)
 {
     int i;
     int j;
+    int temp_pos;
 
-    while (s1[i] != '\0' || s2[j] != '\0')
-    {
-        if (s1[i] == s2[j])
-        {
-            j++;
-        }
-        else
-        {
-            if (s2[j] == '*')
-            {
-                if (s2[j + 1] == '\0')
-                    return (1);
-                else if (s1[i + 1] == s2[j + 1])
-                {
-                    j++;
-                }
-            }
-            else
-            {
-                return (0);
-            }
-        }
-        i++;
-    }
-    return (1);
+    i = 0;
+    j = 0;
+    return rmatch(s1, s2, i, j);
 }
